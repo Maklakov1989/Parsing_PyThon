4d700155f3b954d9d4c7dc378f2d5f3e0a245212
Alexey <maklakov_a.n@mail.ru>
2022-12-15 18:35:28 +0300

    Step_2

--- GitHub_pancing.py
+++ GitHub_pancing.py
@@ -1,8 +1,17 @@
+# print(('*' * 25), 'Задание №1 к лекции № 1', ('*' * 25))
 import requests
-username = 'Maklakov1989'
-request = requests.get('https://api.github.com/users/'+username+'/repos')
-json = request.json()
-for i in range(0,len(json)):
-  print("Project Number:",i+1)
-  print("Project Name:",json[i]['name'])
-  print("Project URL:",json[i]['svn_url'],"\n")
+def git_api():
+  username = 'Maklakov1989'
+  request = requests.get('https://api.github.com/users/'+username+'/repos')
+  json = request.json()
+  for i in range(0,len(json)):
+    print("Project Number:",i+1)
+    print("Project Name:",json[i]['name'])
+    print("Project URL:",json[i]['svn_url'],"\n")
+git_api()
+print(('*' * 25), 'Задание №2 к лекции № 1', ('*' * 25))
+def vk():
+  user_id = '1276429'
+  request = requests.get('https://api.vk.com/method/users.get?groups.get='+user_id+'&v=5.52')
+  print(request)
+vk()
